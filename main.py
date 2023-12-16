bd7b17635ec54bf748f5feaed47e7eb4797a829e
Tomoya Kashimada <kashimada@ntticc.or.jp>
2023-12-16 14:59:36 +0900

    feat: :+1: add module of switchbot

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
-# SwitchBot
-import base64
-import hashlib
-import hmac
-import os
-import requests
-import uuid
+# Main
 from logger import logger
-from pprint import pformat
-from time import sleep, time
+from time import sleep
 
-from dotenv import load_dotenv
 from schedule import every, run_pending
-
-load_dotenv()
-
-# Endpoint
-url = "https://api.switch-bot.com"
-# Declare empty header dictionary
-apiHeader = {}
+from switchbot import initialize_auth, post_command, get_devices, get_statuses
 
 # Device IDs
 plug = "6055F93C4C3A"
@@ -26,50 +12,7 @@ bot = "C9864F52A4D7"
 on = "turnOn"
 off = "turnOff"
 
-def initialize_auth():
-    token = os.getenv("TOKEN")
-    secret = os.getenv("SECRET")
-    nonce = uuid.uuid4()
-    t = int(round(time() * 1000))
-    sign = base64.b64encode(
-        hmac.new(
-            bytes(secret, "utf-8"),
-            msg=bytes("{}{}{}".format(token, t, nonce), "utf-8"),
-            digestmod=hashlib.sha256).digest()
-    )
-
-    #Build api header JSON
-    apiHeader["Authorization"]=token
-    apiHeader["Content-Type"]="application/json"
-    apiHeader["charset"]="utf8"
-    apiHeader["t"]=str(t)
-    apiHeader["sign"]=str(sign, "utf-8")
-    apiHeader["nonce"]=str(nonce)
-    logger.debug(pformat(apiHeader))
-
-
-def get_devices():
-    initialize_auth()
-    res = requests.get(f"{url}/v1.1/devices", headers=apiHeader)
-    logger.debug(pformat(res.json()))
-
-
-def get_statuses(device_id: str):
-    initialize_auth()
-    res = requests.get(f"{url}/v1.1/devices/{device_id}/status", headers=apiHeader)
-    logger.debug(pformat(res.json()))
-
-
-def post_command(device_id: str, command: str):
-    request_body = {
-        "command": command,
-        "parameter": "default",
-        "commandType": "command"
-    }
-    res = requests.post(f"{url}/v1.1/devices/{device_id}/commands", headers=apiHeader, json=request_body)
-    logger.debug(pformat(res.json()))
-
-
+# Control Record Player
 def play():
     initialize_auth()
     post_command(plug, on)
@@ -85,8 +28,6 @@ def stop():
     post_command(plug, off)
     logger.info("Stop.")
 
-
-# Main
 logger.info("Start auto play record player.")
 get_devices()
 
---
+++ switchbot.py
@@ -0,0 +1,62 @@
+# SwitchBot
+import base64
+import hashlib
+import hmac
+import os
+import requests
+import uuid
+from logger import logger
+from pprint import pformat
+from time import time
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Endpoint
+url = "https://api.switch-bot.com"
+# Declare empty header dictionary
+apiHeader = {}
+
+def initialize_auth():
+    token = os.getenv("TOKEN")
+    secret = os.getenv("SECRET")
+    nonce = uuid.uuid4()
+    t = int(round(time() * 1000))
+    sign = base64.b64encode(
+        hmac.new(
+            bytes(secret, "utf-8"),
+            msg=bytes("{}{}{}".format(token, t, nonce), "utf-8"),
+            digestmod=hashlib.sha256).digest()
+    )
+
+    #Build api header JSON
+    apiHeader["Authorization"]=token
+    apiHeader["Content-Type"]="application/json"
+    apiHeader["charset"]="utf8"
+    apiHeader["t"]=str(t)
+    apiHeader["sign"]=str(sign, "utf-8")
+    apiHeader["nonce"]=str(nonce)
+    logger.debug(pformat(apiHeader))
+
+
+def get_devices():
+    initialize_auth()
+    res = requests.get(f"{url}/v1.1/devices", headers=apiHeader)
+    logger.debug(pformat(res.json()))
+
+
+def get_statuses(device_id: str):
+    initialize_auth()
+    res = requests.get(f"{url}/v1.1/devices/{device_id}/status", headers=apiHeader)
+    logger.debug(pformat(res.json()))
+
+
+def post_command(device_id: str, command: str):
+    request_body = {
+        "command": command,
+        "parameter": "default",
+        "commandType": "command"
+    }
+    res = requests.post(f"{url}/v1.1/devices/{device_id}/commands", headers=apiHeader, json=request_body)
+    logger.debug(pformat(res.json()))
